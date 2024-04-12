d2fcfbf14fa7668de88dc44d87f335faa5ca0c51
Siddhesh4575 <137050036+Siddhesh4575@users.noreply.github.com>
2024-04-13 01:38:48 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+// Even and odd problem
+    public static void main(String agrs[]) {
+        Scanner Sc = new Scanner(System.in);
+        int x = Sc.nextInt();
+
+        if (x % 2 == 0) {
+            System.out.println("Its Even Number");
+        }
+        else {
+            System.out.println("Its Odd Number");
+        }
+    }
